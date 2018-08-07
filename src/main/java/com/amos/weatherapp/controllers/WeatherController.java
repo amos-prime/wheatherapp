9b1fa6afe105cd876eb807e92fa636b8999ca2e1
Artur Mos <artur.mos@gmail.com>
2018-08-07 16:53:28 +0200

    Drawing charts based on data provided by WeatherController

--- src/main/java/com/amos/weatherapp/controllers/WeatherController.java
+++ src/main/java/com/amos/weatherapp/controllers/WeatherController.java
@@ -1,15 +1,27 @@
 package com.amos.weatherapp.controllers;
 
 import com.amos.weatherapp.model.ForecastEntry;
+import com.amos.weatherapp.model.WeatherForecast;
+import com.amos.weatherapp.services.WeatherForecastService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class WeatherController {
+
+    private WeatherForecastService weatherForecastService;
+
+    @Autowired
+    public WeatherController(WeatherForecastService weatherForecastService) {
+        this.weatherForecastService = weatherForecastService;
+    }
 
     @RequestMapping("/")
     public String redirect() {
@@ -18,6 +30,9 @@ public class WeatherController {
 
     @GetMapping("/forecast")
     public String getWeatherForecast(Model model) {
+        WeatherForecast forcast = weatherForecastService.getForecastFor("london");
+        model.addAttribute("cityName", forcast.getCityName());
+        model.addAttribute("weatherEntries", forcast.getEntries());
         return "forecast";
     }
 }
--- src/test/java/com/amos/weatherapp/controllers/WeatherControllerTest.java
+++ src/test/java/com/amos/weatherapp/controllers/WeatherControllerTest.java
@@ -1,9 +1,11 @@
 package com.amos.weatherapp.controllers;
 
+import com.amos.weatherapp.services.WeatherForecastService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -20,6 +22,9 @@ public class WeatherControllerTest {
 
     @Autowired
     MockMvc mvc;
+
+    @MockBean
+    WeatherForecastService weatherForecastService;
 
     @Test
     public void weatherController_shouldRedirectOnBaseUrl() throws Exception {
